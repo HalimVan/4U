4bdf8bd80412207e2e61f1dd85166d155e68113b
ShizukaAkari <yusrilsyahruddin@gmail.com>
2020-04-15 15:16:52 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -101,7 +101,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "Official Repo [here](https://github.com/adekmaulana/ProjectBish) & this my fork repo [here](https://github.com/yusrilsyahruddin/4U) GitHub page."
+        "[here](https://github.com/yusrilsyahruddin/4U) GitHub page."
     )
 
 
