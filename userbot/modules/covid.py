ce5a1ee19e4593307e1ccbdd71a6b9c901885d43
ShizukaAkari <yusrilsyahruddin@gmail.com>
2020-04-15 15:12:16 +0700

    translate id

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -17,10 +17,10 @@ async def corona(event):
     covid = Covid()
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']}`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text =  f"`Dikonfirmasi: {country_data['confirmed']}`\n"
+        output_text += f"`Aktif       : {country_data['active']}`\n"
+        output_text += f"`meninggal   : {country_data['deaths']}`\n"
+        output_text += f"`pulih       : {country_data['recovered']}`\n"
         output_text += (
             "`Last update : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
