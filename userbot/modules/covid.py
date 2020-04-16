0dda9f7b731a7e6ecc0bb1fc6f4d4d5f27d50ea4
ShizukaAkari <yusrilsyahruddin@gmail.com>
2020-04-16 13:48:04 +0700

    Update covid.py

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -12,21 +12,21 @@ from userbot.events import register
 
 @register(outgoing=True, pattern="^.covid (.*)")
 async def corona(event):
-    await event.edit("Processing...")
+    await event.edit("`Processing...`")
     country = event.pattern_match.group(1)
     covid = Covid()
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text += f"`Dikonfirmasi: {country_data['confirmed']}`\n"
-        output_text += f"`Aktif       : {country_data['active']}`\n"
-        output_text += f"`meninggal   : {country_data['deaths']}`\n"
-        output_text += f"`pulih       : {country_data['recovered']}`\n"
+        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
+        output_text += f"`Active      : {country_data['active']}`\n"
+        output_text += f"`Deaths      : {country_data['deaths']}`\n"
+        output_text += f"`Recovered   : {country_data['recovered']}`\n"
         output_text += (
-            "`Pembaruan Terakhir : "
+            "`Last update : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
         )
-        output_text += f"Data provided by [Johns Hopkins University](https://j.mp/2xf6oxF)"
-        else:
+        output_text += f"Data provided by [YusrilSyahruddin](https://github.com/yusrilsyahruddin/4U)"
+    else:
         output_text = "No information yet about this country!"
     await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
 
