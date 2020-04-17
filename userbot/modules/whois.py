5872619f5678539719c1f018ff9437d9383039c2
ShizukaAkari <yusrilsyahruddin@gmail.com>
2020-04-18 04:05:14 +0700

    translate id

--- userbot/modules/whois.py
+++ userbot/modules/whois.py
@@ -21,7 +21,7 @@ from userbot.events import register
 async def who(event):
 
     await event.edit(
-        "`Sit tight while I steal some data from *Global Network Zone*...`")
+        "`Duduk tenang sementara aku mencuri beberapa data dari *Zona Jaringan Global**...`")
 
     if not os.path.isdir(TEMP_DOWNLOAD_DIRECTORY):
         os.makedirs(TEMP_DOWNLOAD_DIRECTORY)
