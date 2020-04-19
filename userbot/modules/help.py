ec154e4731871117086a0f0a8a34859b50ee2fc1
ShizukaAkari <yusrilsyahruddin@gmail.com>
2020-04-20 00:16:48 +0700

    Update help.py

--- userbot/modules/help.py
+++ userbot/modules/help.py
@@ -12,6 +12,15 @@ from userbot.events import register
 @register(outgoing=True, pattern="^.help(?: |$)(.*)")
 async def help(event):
     """ For .help command,"""
+    args = event.pattern_match.group(1).lower()
+    if args:
+        if args in CMD_HELP:
+            await event.edit(str(CMD_HELP[args]))
+        else:
+            await event.edit("Silakan nyatakan nama modul yang valid.")
+    else:
+        await event.edit("Silakan nyatakan modul mana yang Anda inginkan !!"
+                         "\nPenggunaan: .help <nama modul>")
         string = "-  "
         for i in CMD_HELP:
             string += "`" + str(i)
