fbeb5e92d538edb5a073d10ea190d4b6015c1c87
ShizukaAkari <yusrilsyahruddin@gmail.com>
2020-04-19 12:14:00 +0700

    Update covid.py

--- userbot/modules/covid.py
+++ userbot/modules/covid.py
@@ -17,22 +17,22 @@ async def corona(event):
     covid = Covid()
     country_data = covid.get_status_by_country_name(country)
     if country_data:
-        output_text =  f"`Confirmed   : {country_data['confirmed']}`\n"
-        output_text += f"`Active      : {country_data['active']}`\n"
-        output_text += f"`Deaths      : {country_data['deaths']}`\n"
-        output_text += f"`Recovered   : {country_data['recovered']}`\n"
+        output_text =  f"`Dikonfirmasi : {country_data['confirmed']}`\n"
+        output_text += f"`Aktif        : {country_data['active']}`\n"
+        output_text += f"`Meninggal    : {country_data['deaths']}`\n"
+        output_text += f"`Pulih.       : {country_data['recovered']}`\n"
         output_text += (
             "`Last update : "
             f"{datetime.utcfromtimestamp(country_data['last_update'] // 1000).strftime('%Y-%m-%d %H:%M:%S')}`\n"
         )
-        output_text += f"Data provided by [YusrilSyahruddin](https://github.com/yusrilsyahruddin/4U)"
+        output_text += f"Data yang disediakan oleh YusrilSyahruddin](https://www.instagram.com/yuskychan/?hl=id)"
     else:
-        output_text = "No information yet about this country!"
+        output_text = "Tulis nama negara dengan benar BANGSAT!"
     await event.edit(f"Corona Virus Info in {country}:\n\n{output_text}")
 
 
 CMD_HELP.update({
         "covid": 
-        ".covid <country>"
-        "\nUsage: Get an information about data covid-19 in your country.\n"
+        ".covid <negara>"
+        "\nPenggunaan: mendapatkan informasi tentang data covid-19 di negara Anday.\n"
     })
